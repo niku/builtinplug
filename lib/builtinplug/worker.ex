45ad715e3177dcf6c5f9870a456aa1332b5585a6
niku <niku@niku.name>
2015-07-02 20:26:43 +0900

    We can pry like this code with `iex -S mix`

--- lib/builtinplug/worker.ex
+++ lib/builtinplug/worker.ex
@@ -1,13 +1,16 @@
 defmodule Builtinplug.Worker do
   import Plug.Conn
 
-  def init(options) do
+  def init(_options) do
     # initialize options
 
-    options
+    " (ノ-_-)ノ~┻┻"
   end
 
   def call(conn, _opts) do
+    require IEx
+    IEx.pry
+
     conn
     |> put_resp_content_type("text/plain")
     |> send_resp(200, "Hello world")
